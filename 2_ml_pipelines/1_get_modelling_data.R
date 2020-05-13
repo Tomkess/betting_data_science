53e2da489a238777a89f12118e297f1452206d6d
Peter Tomko <tomko5peter@gmail.com>
2020-05-13 10:02:08 +0200

    prototype. implement improved speed of modelling data computation. add writing of files into folder. will be changed for more optimal solution.

--- 2_ml_pipelines/1_get_modelling_data.R
+++ 2_ml_pipelines/1_get_modelling_data.R
@@ -3,45 +3,86 @@ library(dplyr)
 library(data.table)
 library(config)
 
-# ----- Load Team Mapping -----
-dest_path <- "1_variable_calculator/db_temp/1_variable_calculator.RData"
-load(paste(get("working_directory")[["4finance_PC"]], 
-           "/", dest_path, sep = ""))
+# ----- Set the Working Directory -----
+setwd("C:/Users/Peter/Desktop/ds_projects/betting_data_science")
+var_files <- data.frame("files" = list.files("1_variable_calculator/db_temp"))
+var_files$league_name <- 
+  stringr::str_remove_all(stringr::str_remove_all(var_files$files, pattern = ".RData"), 
+                          "1_variable_calculator_")
 
 # ----- Load Predictors Function -----
-source(paste(get("working_directory")[["4finance_PC"]], 
-             "/2_ml_pipelines/0_predictors_function.R", sep = ""))
+source("2_ml_pipelines/0_predictors_function.R")
+gc()
 
 # ----- Compute Modelling Data -----
-output_modelling <- list()
-count <- 1
-for(j in unique(predictors_data$league)){
+for(j in var_files$league_name){
   # j <- "E0"
-  # - Compute modelling data for selected league
-  modelling_temp <- 
-    get_modellingdata(predictors_data %>% 
-                        filter(league %in% j)) %>%
-    left_join(., match_data %>% 
-                select(created_at, is_home, team, match_result, n_goals) %>%
-                as.data.frame(), 
-              by = c("match_date" = "created_at", 
-                     "team" = "team", 
-                     "is_home" = "is_home")) %>%
-    select(-sport) %>%
-    as.data.frame()
   
-  if(count == 1){
-    modelling_data <- modelling_temp %>% as.data.frame()
-    count <- count + 1
-  }else{
-    modelling_data <- 
-      modelling_data %>%
-      rbind(., modelling_temp) %>%
+  # - get already computed leagues
+  already_computed <- list.files("2_ml_pipelines/db_temp")
+  already_computed <- 
+    already_computed[stringr::str_detect(already_computed, 
+                                         pattern = ".RData") == TRUE]
+  already_computed <- 
+    already_computed[stringr::str_detect(already_computed, 
+                                         pattern = "_data_") == TRUE]
+  already_computed <- 
+    stringr::str_remove_all(stringr::str_remove_all(already_computed, 
+                                                    pattern = ".RData"), "modelling_data_")
+  
+  print(j)
+  if(!(j %in% already_computed)){
+    # ----- Load Team Mapping -----
+    load(paste("1_variable_calculator/db_temp/1_variable_calculator_", j, ".RData", sep = ""))
+    
+    # j <- "EC"
+    # - Compute modelling data for selected league
+    modelling_temp <- 
+      get_modellingdata(predictors_data %>%
+                          filter(league %in% j)) %>%
+      left_join(., match_data %>% 
+                  select(created_at, is_home, team, match_result, n_goals) %>%
+                  as.data.frame(), 
+                by = c("match_date" = "created_at", 
+                       "team" = "team", 
+                       "is_home" = "is_home")) %>%
+      select(-sport) %>%
       as.data.frame()
     
-    count <- count + 1
+    rm(predictors_data)
+    gc()
+    
+    # - save immediately in folder
+    save(modelling_temp, 
+         file = paste("2_ml_pipelines/db_temp/modelling_data_", 
+                      j, ".RData", sep = ""))
   }
   
-  # - save immediately in folder
-  save(modelling_data, file = "2_ml_pipelines/db_temp/modelling_data.RData")
+  print(j)
 }
+
+# - get already computed leagues
+already_computed <- list.files("2_ml_pipelines/db_temp")
+already_computed <- 
+  already_computed[stringr::str_detect(already_computed, 
+                                       pattern = ".RData") == TRUE]
+already_computed <- 
+  already_computed[stringr::str_detect(already_computed, 
+                                       pattern = "_data_") == TRUE]
+already_computed <- 
+  stringr::str_remove_all(stringr::str_remove_all(already_computed, 
+                                                  pattern = ".RData"), "modelling_data_")
+
+o_modelling_data <- list()
+for(i in already_computed){
+  load(paste("2_ml_pipelines/db_temp/modelling_data_", i, ".RData", sep = ""))
+  
+  o_modelling_data[[i]] <- modelling_temp
+}
+
+modelling_data <- o_modelling_data %>% bind_rows()
+rm(list = ls()[!(ls() %in% "modelling_data")])
+gc()
+
+save(modelling_data, 
+     file = "2_ml_pipelines/db_temp/modelling_data.RData")
