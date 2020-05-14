afe9e1c958619d33a177b297e85df3a673a3b89a
Peter Tomko <tomko5peter@gmail.com>
2020-05-14 16:56:36 +0200

    select only the most recent matches. deselect the suspicious matches - wrong naming in the database.

--- 3_upcoming_matches/0_nearest_match.R
+++ 3_upcoming_matches/0_nearest_match.R
@@ -6,7 +6,7 @@ library(tidyr)
 library(stringr)
 
 # ----- Set Working Directory -----
-setwd("C:/Users/Peter.Tomko/OneDrive - 4Finance/concept/Betting Data Science")
+setwd("C:/Users/Peter/Desktop/ds_projects/betting_data_science")
 
 # ----- Load Actual XML Feed -----
 load("0_etl/db_temp/1_tipsport_feedxml.RData")
@@ -15,91 +15,115 @@ load("0_etl/db_temp/1_tipsport_feedxml.RData")
 load("2_ml_pipelines/-unique_value_check/mapping_storage/team_mapping.RData")
 
 # ----- Set Current Date ----- #
-current_date <- Sys.Date() - 120
+current_date <- Sys.Date() - 12
 
 xml_data <- 
   data_temp %>%
-  filter(sport_league %in% unique(team_mapping$Tipsport_Changed) & 
-           sport_category %in% "Fotbal - muži" &
-           eventname %in% c("Výsledek zápasu", "Počet gólů v zápasu") &
-           str_detect(fullname, pattern = "\\(") == F) %>%
+  filter(sport_league %in% unique(team_mapping$Tipsport_Changed)) %>%
+  filter(sport_category %in% "Fotbal - muži" &
+           (eventtypedescription %in% c("Vítěz", "Počet gólů (bodů)"))) %>%
   mutate(dateclosed = dmy_hm(dateclosed)) %>%
   as.data.frame() %>%
   
   filter(dateclosed >= current_date) %>%
-  as.data.frame() %>%
+  as.data.frame()
+
+winner_bets <- 
+  xml_data %>% 
+  filter(type %in% c("1", "2", "x") & eventname %in% "Výsledek zápasu") %>%
   
-  select(-eventid, -eventtype, -opportunityid, -number, -gamepart, 
-         -eventtypedescription, -matchid, -livematchid, -url, -urlold,
-         -sport_id, -sport_category, -sport, -annualname, 
-         -competition_name, -eventname) %>%
-  as.data.frame() %>%
-  
-  rowwise() %>%
-  mutate(HomeTeam = trimws(str_split(namefull, "-")[[1]][1], which = "both"),
-         AwayTeam = trimws(str_split(namefull, "-")[[1]][2], which = "both")) %>%
-  as.data.frame() %>%
-  
-  rowwise() %>%
   mutate(type_match = ifelse(type %in% "1", "tipsportH", 
                              ifelse(type %in% "x", "tipsportD", 
-                                    ifelse(type %in% "2", "tisportA", fullname)))) %>%
-  rename(match_name = namefull) %>%
-  select(-type, -fullname) %>%
+                                    ifelse(type %in% "2", "tipsportA", fullname)))) %>%
+  
+  select(-one_of("eventtype", "eventtypedescription", "gamepart", "eventid", 
+                 "eventname", "opportunityid", "number", "fullname", "sport_id", 
+                 "sport_category", "sport", "livematchid", "annualname", 
+                 "url", "urlold", "matchid", "sport_url", "sport_urlold")) %>%
+  select(-type) %>%
+  
+  # - get newest bets
+  group_by(dateclosed, namefull, competition_name, sport_league, type_match) %>%
+  mutate(max_createdat = max(created_at)) %>%
   as.data.frame() %>%
+  filter(created_at == max_createdat) %>%
+  select(-max_createdat) %>%
+  
   spread(type_match, rate) %>%
+  rowwise() %>%
+  mutate(HomeTeam = trimws(str_split(competition_name, "-")[[1]][1], which = "both"),
+         AwayTeam = trimws(str_split(competition_name, "-")[[1]][2], which = "both")) %>%
   
   as.data.frame() %>%
   left_join(., team_mapping %>% 
-              select(Tipsport_Changed, 
-                     `Tipsport Team [Changed]`, `Results Team`) %>%
+              select(Tipsport_Changed, `Tipsport Team [Changed]`, 
+                     `Results Team`, `Results League`) %>%
               distinct(), 
             by = c("sport_league" = "Tipsport_Changed", 
                    "HomeTeam" = "Tipsport Team [Changed]")) %>%  
-  rename(HomeTeam_tipsport = `Results Team`) %>%
+  rename(HomeTeam_tipsport = `Results Team`,
+         HomeTeam_League = `Results League`) %>%
   
   left_join(., team_mapping %>% 
               select(Tipsport_Changed, `Tipsport Team [Changed]`, 
-                     `Results Team`) %>%
+                     `Results Team`, `Results League`) %>%
               distinct(), 
             by = c("sport_league" = "Tipsport_Changed", 
-                   "HomeTeam" = "Tipsport Team [Changed]")) %>%  
-  rename(AwayTeam_tipsport = `Results Team`) %>%
-  as.data.frame()
-
-# - Duplicates might appear!!!! (same team changes the name over time!!!)
-
-# - calculate mean for English Premier League
-# group_by(dateclosed, match_name, sport_league, sport_url, sport_urlold, 
-#          HomeTeam, AwayTeam, HomeTeam_tipsport, AwayTeam_tipsport) %>%
-# summarise_all(., mean, na.rm = TRUE) %>%
-# filter(sport_league %in% "1. anglická liga") %>%
-# as.data.frame()
-
-nearest_matches <-
-  xml_data %>%
-  select(HomeTeam, dateclosed) %>%
-  group_by(HomeTeam) %>%
-  mutate(min_date = min(dateclosed)) %>%
-  rename(team_input = HomeTeam) %>%
+                   "AwayTeam" = "Tipsport Team [Changed]")) %>%  
+  rename(AwayTeam_tipsport = `Results Team`,
+         AwayTeam_League = `Results League`) %>%
+  as.data.frame() %>%
   
-  rbind(., xml_data %>%
-          select(AwayTeam, dateclosed) %>%
-          group_by(AwayTeam) %>%
-          mutate(min_date = min(dateclosed)) %>%
-          rename(team_input = AwayTeam)) %>%
-  group_by(team_input) %>%
-  summarise(min_date = min(min_date))
+  select(-competition_name)
 
-xml_data <- xml_data %>%
-  # - get nearest match of HomeTeam
-  left_join(., nearest_matches, by = c("HomeTeam" = "team_input")) %>%
-  rename(HomeTeam_mindate = min_date) %>%
+goals_number <- xml_data %>%
+  filter(type %in% c("u", "o") & gamepart %in% "1/1") %>%
+  select(-one_of("eventtype", "eventtypedescription", "gamepart", "eventid", 
+                 "eventname", "opportunityid", "number", "sport_id", "type",
+                 "sport_category", "sport", "livematchid", "annualname", 
+                 "url", "urlold", "matchid", "sport_url", "sport_urlold")) %>%
+  distinct() %>%
   
-  # - get nearest match of AwayTeam
-  left_join(., nearest_matches, by = c("AwayTeam" = "team_input")) %>%
-  rename(AwayTeam_mindate = min_date)
+  # - get newest bets
+  group_by(fullname, dateclosed, namefull, competition_name, sport_league) %>%
+  mutate(max_createdat = max(created_at)) %>%
+  as.data.frame() %>%
+  filter(created_at == max_createdat) %>%
+  select(-max_createdat) %>%
+  as.data.frame() %>%
+  
+  spread(fullname, rate)
 
-xml_data <- xml_data %>%
-  filter(HomeTeam_mindate == AwayTeam_mindate) %>%
-  select(-HomeTeam_mindate, -AwayTeam_mindate)
+nearest_matches <- winner_bets %>%
+  left_join(., goals_number)
+
+min_schedule <- 
+  nearest_matches %>%
+  select(HomeTeam_tipsport, dateclosed) %>%
+  distinct() %>%
+  group_by(HomeTeam_tipsport) %>%
+  mutate(min_dateclosed = min(dateclosed)) %>%
+  rename(team = HomeTeam_tipsport) %>%
+  
+  rbind(., nearest_matches %>%
+          select(AwayTeam_tipsport, dateclosed) %>%
+          distinct() %>%
+          group_by(AwayTeam_tipsport) %>%
+          mutate(min_dateclosed = min(dateclosed)) %>%
+          rename(team = AwayTeam_tipsport)) %>%
+  group_by(team) %>%
+  summarise(ult_min_dateclosed = min(min_dateclosed))
+
+nearest_matches <- nearest_matches %>%
+  left_join(., min_schedule, by = c("HomeTeam_tipsport" = "team")) %>%
+  rename(hometeam_mindateclosed = ult_min_dateclosed) %>%
+  
+  left_join(., min_schedule, by = c("AwayTeam_tipsport" = "team")) %>%
+  rename(awayteam_mindateclosed = ult_min_dateclosed)
+
+nearest_matches <- nearest_matches %>%
+  filter(dateclosed == awayteam_mindateclosed & 
+           dateclosed == hometeam_mindateclosed)
+
+rm(list = ls()[!(ls() %in% "nearest_matches")])
+save.image("3_upcoming_matches/db_temp/nearest_matches.RData")
