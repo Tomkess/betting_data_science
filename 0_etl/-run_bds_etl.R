89b0a0cb800650e7bdaa9b376b32ab58577f72b6
Peter Tomko <tomko5peter@gmail.com>
2020-05-14 14:03:36 +0200

    change the file path logic.

--- 0_etl/-run_bds_etl.R
+++ 0_etl/-run_bds_etl.R
@@ -10,20 +10,7 @@ rm(list = ls())
 gc()
 
 # ----- Set the Working Directory -----
-if(Sys.info()[['nodename']] %in% c("966916-dci1-adw-002.ofg.local")){
-  # - path on server
-  setwd("/home/peter.tomko/concept---data-science")
-}
-
-if(Sys.info()[['nodename']] %in% c("CZIT-NTB-017")){
-  # - path on 4finance ntb
-  setwd("C:/Users/Peter.Tomko/OneDrive - 4Finance/concept/Betting Data Science")
-}
-
-if(Sys.info()[['nodename']] %in% c("PETER")){
-  # - path on private ntb
-  setwd("C:/Users/Peter/Desktop/ds_projects/betting_data_science")
-}
+setwd("C:/Users/Peter/Desktop/ds_projects/betting_data_science")
 
 # ------ Sourcing the Scripts ------
 source("0_etl/0_results_download.R")
