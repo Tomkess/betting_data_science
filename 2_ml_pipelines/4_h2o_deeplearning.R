b53466877d7a191b4969a27037afcb4a1f1554be
Peter Tomko <tomko5peter@gmail.com>
2020-08-23 19:52:17 +0200

    h2o model adjustment

--- 2_ml_pipelines/4_h2o_deeplearning.R
+++ 2_ml_pipelines/4_h2o_deeplearning.R
@@ -5,59 +5,42 @@ library(h2o)
 h2o.init()
 
 # ----- Set Working Directory -----
-setwd("C:/Users/Peter.Tomko/OneDrive - 4Finance/concept/Betting Data Science")
+setwd("C:/Users/Peter/Desktop/ds_projects/betting_data_science")
 
 # ----- Load Modelling and Match Data -----
-load("2_ml_pipelines/db_temp/modelling_data.RData")
-load("1_variable_calculator/db_temp/1_variable_calculator.RData")
-
-rm(predictors_data)
-
-# ----- Model Configuration -----
-test_date <- "2018-06-01"
-train_date <- "2018-01-01"
-
-# ----- Create Weighting Structure -----
-modelling_data <- modelling_data %>%
-  left_join(., match_data %>%
-              group_by(created_at, is_home, team) %>%
-              summarise(m_weights = abs(mean(r_bookmakers_fee))),
-            by = c("match_date" = "created_at", 
-                   "is_home" = "is_home", 
-                   "team" = "team"))
-
-# ----- Model COnfiguration -----
-test_date <- "2018-06-01"
-train_date <- "2018-01-01"
+load("2_ml_pipelines/db_temp/train_data.RData")
+load("2_ml_pipelines/db_temp/test_data.RData")
 
 # ----- Fit deeplearning model using h2o -----
 traindata <- 
-  modelling_data %>%
-  filter(match_date <= as.Date(train_date)) %>%
-  dplyr::select(-match_date, -match_result, -team, -league) %>%
+  train_woe %>%
+  mutate(n_goals_dummy = ifelse(n_goals > 2.5, "Over 2.5", "Under 2.5")) %>%
+  dplyr::select(-match_date, -match_result, -team, -n_goals) %>%
+  mutate_if(is.character, factor) %>%
   as.data.frame()
 
 valdata <- 
-  modelling_data %>%
-  filter(match_date > as.Date(train_date) & 
-           match_date < as.Date(test_date)) %>%
-  dplyr::select(-match_date, -match_result, -team, -league) %>%
+  test_woe %>%
+  mutate(n_goals_dummy = ifelse(n_goals > 2.5, "Over 2.5", "Under 2.5")) %>%
+  dplyr::select(-match_date, -match_result, -team, -n_goals) %>%
+  mutate_if(is.character, factor) %>%
   as.data.frame()
 
 traindata_h2o <- as.h2o(traindata)
 valdata_h2o <- as.h2o(valdata)
 
-model_h2o <- h2o.deeplearning(x = 1:261, y = 262, 
-                              training_frame = traindata_h2o, 
-                              seed = 123456, weights_column = "m_weights", 
-                              validation_frame = valdata_h2o, 
-                              keep_cross_validation_predictions = T, 
-                              nfolds = 35)
+fit_model_h2o <- h2o.deeplearning(x = 1:41, y = 42, 
+                                  training_frame = traindata_h2o, 
+                                  seed = 123456, 
+                                  validation_frame = valdata_h2o, 
+                                  keep_cross_validation_predictions = T, 
+                                  nfolds = 50, balance_classes = T, 
+                                  elastic_averaging = T, 
+                                  stopping_metric = "logloss",
+                                  #fold_assignment = "Stratified",
+                                  standardize = T)
 
-# h2o.mae(object = model_h2o, valid = T, train = T)
-# h2o.mse(object = model_h2o, valid = T, train = T)
-
-# prediction_data <- as.numeric(as.data.frame(h2o.predict(object = model_h2o, 
-#                                              newdata = testdata_h2o))$predict)
-
-model_h2o <- h2o.saveModel(object = model_h2o, path = "2_ml_pipelines/db_temp")
+h2o.performance(fit_model_h2o, traindata_h2o)
+h2o.performance(fit_model_h2o, valdata_h2o)
+model_h2o <- h2o.saveModel(object = fit_model_h2o, 
+                           path = "2_ml_pipelines/db_temp")
