3a1eb210da2b6f0a235ab57402f0dbe433003836
Peter Tomko <peter.tomko@aaaauto.cz>
2021-02-27 16:44:40 +0100

    run data refresh

---
+++ run.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/Peter/Desktop/ds_projects/betting_data_science/")
+
+# ----- Refresh Data -----
+source("./2 data preparation/run - Football Data co.R")
+source("./2 data preparation/run - Tipsport XML Feed.R")
