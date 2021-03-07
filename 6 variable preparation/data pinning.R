f3da65d83cbb46c936367b1c7a9d7d2cbd5027f9
Peter Tomko <peter.tomko@aaaauto.cz>
2021-03-07 20:39:59 +0100

    include refresh views before pinning - the table are droped each time when refreshing the data and therefore the views are not working. refresh is necessary

--- 6 variable preparation/data pinning.R
+++ 6 variable preparation/data pinning.R
@@ -4,6 +4,9 @@ library("generalToolboxR")
 # ----- Settin Working Directory -----
 setwd("C:/Users/Peter/Desktop/ds_projects/betting_data_science/")
 
+# ----- Refresh Views -----
+source("./3 refresh db structure/refresh views.R")
+
 # ----- Connect to Database -----
 betting_ds <- dbManager$new()
 betting_ds$db_config(db_drv = "PostgreSQL",
@@ -40,3 +43,57 @@ target_vars <-
 t_mapping_team <- 
   betting_ds$run(sql_path = "./db/Scripts/get mapping team.sql") %>% 
   pin("t_mapping_team", board = "local")
+
+# ----- Results Data -----
+results_data <-
+  pin_get("v_match_stats", "local") %>% 
+  as.data.frame() %>% 
+  distinct() %>%
+  
+  dplyr::select(season, league, created_at, team, is_home, 
+                match_id, total_goals, n_goals) %>% 
+  as.data.frame() %>%
+  
+  left_join(., pin_get("t_mapping_team", "local") %>% 
+              as.data.frame(),
+            by = c("team" = "results_team",
+                   "league" = "results_league")) %>% 
+  as.data.frame() %>% 
+  
+  dplyr::select(-team, -league) %>% 
+  distinct() %>% 
+  as.data.frame() %>% 
+  
+  dplyr::filter(is_home == 1) %>% 
+  rename(home_team = tipsport_team,
+         h_goals = n_goals) %>%
+  dplyr::select(-is_home) %>% 
+  
+  left_join(., pin_get("v_match_stats", "local") %>% 
+              as.data.frame() %>% 
+              distinct() %>%
+              
+              dplyr::select(season, league, created_at, team, is_home, 
+                            match_id, total_goals, n_goals) %>% 
+              as.data.frame() %>%
+              
+              left_join(., pin_get("t_mapping_team", "local") %>% 
+                          as.data.frame(),
+                        by = c("team" = "results_team",
+                               "league" = "results_league")) %>% 
+              as.data.frame() %>% 
+              
+              dplyr::select(-team, -league) %>% 
+              distinct() %>% 
+              as.data.frame() %>% 
+              
+              dplyr::filter(is_home == 0) %>% 
+              rename(away_team = tipsport_team,
+                     a_goals = n_goals) %>%
+              dplyr::select(-is_home, -total_goals) %>% 
+              as.data.frame()) %>% 
+  
+  na.omit() %>% 
+  dplyr::select(-match_id) %>% 
+  as.data.frame() %>% 
+  pin("results_data", "local")
